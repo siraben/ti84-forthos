872732c5a08572569bf815c65f153cde9a6aa270
siraben <bensiraphob@gmail.com>
2018-12-07 12:02:35 +0700

    Created branch with single file ASM os.

--- include/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DISPLAY_H
-#define _DISPLAY_H
-
-#define DISPLAY_WIDTH 96
-#define DISPLAY_HEIGHT 64
-
-void display_copy(uint8_t *screen);
-
-#endif
--- include/flash.asm
+++
@@ -1,7 +0,0 @@
-#ifndef _FLASH_IMPORTS
-#define _FLASH_IMPORTS
-
-.import unlock_flash
-.import lock_flash
-
-#endif
