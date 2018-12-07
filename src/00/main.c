0c651da29d7616bfa0104f85d10785dff9b73a6f
siraben <bensiraphob@gmail.com>
2018-12-07 15:49:50 +0700

    Display and C compiler demo

--- src/00/main.c
+++ src/00/main.c
@@ -1,25 +1,57 @@
 #include <stdint.h>
 #include <display.h>
 
+int mod(int a, int b) {
+  if (a < b) {
+    return a;
+  } else {
+    return mod(a - b, b);
+  }
+}
+int pos(int row, int col) {
+  return (row * DISPLAY_WIDTH / 8 + col);
+}
+
+int mul(int x, int y) {
+  if (x == 1) {
+    return y;
+  } else if (y == 1) {
+    return x;
+  }
+  int res = 0;
+  while(y != 0) {
+    res += x;
+    y--;
+  }
+  return res;
+}
+int prime(int n) {
+  if (n < 3) {
+    return (n == 2) ? 1 : 0;
+  } else {
+    for (int x = 2; mul(x,x) <= n; x++) {
+      if (mod(n, x) == 0) {
+        return 0;
+      }
+    }
+    return 1;
+  }
+}
 void main() {
 	uint8_t *buffer = (uint8_t *)0x8000;
-	uint8_t mask = 0xAA;
+	uint8_t mask = 0;
 	int row, col;
 
 	for (row = 0; row < DISPLAY_HEIGHT; ++row) {
 		for (col = 0; col < DISPLAY_WIDTH / 8; ++col) {
-			buffer[row * DISPLAY_WIDTH / 8 + col] = mask;
-		}
-		if ((mask & 1)) {
-			mask = mask << 1;
-		} else {
-			mask = mask << 1 | 1;
+                  buffer[pos(row, col)] = prime(pos(row, col) >> 2) ? 0xff : 0;
 		}
 	}
 
 	display_copy(buffer);
 
 	while (1) {
-		// Do nothing
+          display_copy(buffer);
+          buffer[0]++;
 	}
 }
